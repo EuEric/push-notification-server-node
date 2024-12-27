4c487f546e6e6bc609d679224a19b9c59e534ce5
EuEric <ericabraham8@yahoo.com>
2024-12-27 21:37:34 +0100

    setup for both development and production with same env variables

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,10 @@
 import { development } from './knexfile.js'; // Import the 'development' configuration
 import Knex from 'knex';
+import dotenv from 'dotenv'
+//Needed to specify multiple paths, because by default, config will look for a file called .env in the current working directory.
+//For migrations the cwd will be src/db, and generally for running the main app, the cwd will be /src
+dotenv.config({ path: ['../../.env', '../.env'] })
 
-const knex = Knex(development);
+const knex = Knex(process.env.NODE_ENV);
 
 export { knex }; // Export the knex instance
