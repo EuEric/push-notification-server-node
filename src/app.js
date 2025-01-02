ec7e366be829b13e75c61f3d2950c42b2272f3b6
EuEric <ericabraham8@yahoo.com>
2025-01-02 17:53:10 +0100

    add separate workflow for dev environment

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import { initializeApp, applicationDefault } from 'firebase-admin/app';
+import dotenv from 'dotenv'
+//Needed to specify multiple paths, because by default, config will look for a file called .env in the current working directory.
+//For migrations the cwd will be src/db, and generally for running the main app, the cwd will be /src
+dotenv.config({ path: ['../../.env', '../.env'] })
 
 // Initialize Firebase app
 initializeApp({
@@ -20,10 +24,13 @@ import notificationsRoutes from './routes/api/notifications.js';
 // Import devices routes
 import devicesRoutes from './routes/api/devices.js';
 
+// Determine the prefix based on the environment
+const devPrefix = process.env.NODE_ENV === 'development' ? '/dev/' : '';
+
 // Mount the notifications routes
-app.use('/api/notifications', notificationsRoutes);
+app.use(`${devPrefix}api/notifications`, notificationsRoutes);
 //Mount the devices routes
-app.use('/api/devices', devicesRoutes);
+app.use(`${devPrefix}api/devices`, devicesRoutes);
 
 // Start the server
 app.listen(port, () => {
