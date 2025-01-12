744a62fcf5d5523d21f8d075c2bf32d2f8229023
EuEric <ericabraham8@yahoo.com>
2025-01-12 17:12:48 +0100

    finish devices api

--- src/routes/api/devices.js
+++ src/routes/api/devices.js
@@ -1,88 +1,105 @@
 import express from 'express';
-import { knex } from '../../db/db.js'
+import { Device } from '../../models/device.js'
+import { Account } from '../../models/account.js'
+import { verifyToken } from '../../middleware/token_verify.js';
+import {createOwnershipMiddleware} from '../../middleware/ownership.js';
 import constants  from '../../constants/api_constants.js';
+import { isHexadecimal } from 'validator';
 const router = express.Router();
 
-//TODO: modify this as well
+const verifyDeviceOwnership = createOwnershipMiddleware({
+  model: Device,
+  ownershipField: 'account_id',
+});
 
-router.post('', async (req, res) => {
-  // TODO: Implementation
+router.post('', verifyToken, async (req, res) => {
   try {
-  const [id] = await  knex('devices')
-  .insert({
-    token: req.body['token'],
-    identifier: req.body['identifier']
-  });
-  res.json({ success: true, id });
+    const identifier = req.body.identifier;
+
+    //Validate input fields
+    if(!identifier || !isHexadecimal(identifier) || !identifier.length == 8) {
+      return res.status(400).json({success: false, message: constants.inputFieldError});
+    }
+    const account = await Account.query().findOne({ email: req.account.email }).withGraphFetched('devices');
+
+    for (const device of account.devices) {
+      if (device.identifier == identifier) {
+        return res.status(400).json({ success: false, message: constants.duplicateFoundError });
+      }
+    }
+   
+    const device = await Device.query().insert({ identifier, account_id: account.id});
+    return res.json({ success: true, device });
   } catch(e) {
     if (e.code === 'ER_DUP_ENTRY') {
       return res.json({success: false, message: constants.duplicateFoundError});
     }
-    console.log(e);
     res.json({success: false, message: constants.internalServerError});
   }
 });
 
-router.get('', (req, res) => {
+router.get('', verifyToken, verifyDeviceOwnership, (req, res) => {
   try {
-    knex.select().from('devices').then((devices) => res.json({success: true, devices}));
+    Account.query().findOne({ email: req.account.email }).withGraphFetched('devices').then((account) => res.json({success: true, devices: account.devices ? account.devices : []}));
   } catch(e) {
     res.status(500).json({success: false, message: constants.internalServerError});
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', verifyToken, verifyDeviceOwnership, (req, res) => {
   try {
-    knex('devices')
-      .where('id', req.params.id)
-      .then((device) => res.json({successs:true, device: device[0] ? device[0] : 'No device found'}));
+    Device.query().findOne({id: req.params.id}).then((device) => res.json({success: true, device: device ? device : 
+      constants.noDeviceFound
+    }));
   } catch(e) {
     res.status(500).json({success: false, message: constants.internalServerError});
   }
 });
 
-router.put('/:id', (req, res) => {
-  var id = req.params.id;
-  //TODO: add some verification if input parameters are format and names they should
+router.put('/:id', verifyToken, verifyDeviceOwnership, async (req, res) => {
+  try {
+    var id = req.params.id;
+    const identifier = req.body.identifier;
 
-  knex('devices')
-    .where('id', id)
-    .update({
-      token: req.body['token'],
-      identifier: req.body['identifier']
-    })
-    .then((count) => {
-      if(count === 0) {
-        return res.status(404).json({success: false, message: constants.deviceNotFoundError});
+    //Validate input fields
+    if(!identifier || !isHexadecimal(identifier) || !identifier.length == 8) {
+      return res.status(400).json({success: false, message: constants.inputFieldError});
+    }
+    const account = await Account.query().findOne({ email: req.account.email }).withGraphFetched('devices');
+
+    //Check if device with same identifier already exists
+    for (const device of account.devices) {
+      if (device.id != id && device.identifier == identifier) {
+        return res.status(400).json({ success: false, message: constants.duplicateFoundError });
       }
-      res.json({ success: true, id });
-    })
-    .catch((e) => {
-      if (e.code === 'ER_DUP_ENTRY') {
-        return res.json({success: false, message: constants.duplicateFoundError});
-      }
-      res.json({success: false, message: constants.internalServerError});
-    })
+    }
+
+    const count = await Device.query().findById(id).patch({identifier});
+    if(count == 0) {
+      return res.status(404).json({success: false, message: constants.deviceNotFoundError});
+    }
+    res.json({ success: true, id });
+  } catch(e)  {
+    if (e.code === 'ER_DUP_ENTRY') {
+      return res.json({success: false, message: constants.duplicateFoundError});
+    }
+    res.json({success: false, message: constants.internalServerError});
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  knex('devices')
-    .where('id', req.params.id)
-    .del()
-    .then((count) => {
-      if (count === 0) {
-        return res.status(404).json({success: false, message: constants.deviceNotFoundError});
-      }
-      res.json({success: true, message: constants.deviceDeletionSuccess});
-    })
-    .catch((e) => {
-      if (e.code === 'ER_TRUNCATED_WRONG_VALUE') {
-        return res.status(400).json({success: false, message: constants.inputFieldError});
-      }
-
-      res.status(500).json({success: false, message: constants.internalServerError});
-    });
+router.delete('/:id', verifyToken, verifyDeviceOwnership, async (req, res) => {
+  try {
+    const count = await Device.query().deleteById(req.params.id);
+    if(count == 0) {
+      return res.status(404).json({success: false, message: constants.deviceNotFoundError});
+    }
+    res.json({success: true, message: constants.deviceDeletionSuccess});
+  } catch(e) {
+    if (e.code === 'ER_TRUNCATED_WRONG_VALUE') {
+      return res.status(400).json({success: false, message: constants.inputFieldError});
+    }
+    res.status(500).json({success: false, message: constants.internalServerError});
+  }
 });
-
 
 export default router;
