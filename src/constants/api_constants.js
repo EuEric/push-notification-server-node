58a0065211373169b2bc006d30decdae60d06b0a
EuEric <ericabraham8@yahoo.com>
2025-01-12 23:32:29 +0100

    add extra constants

--- src/constants/api_constants.js
+++ src/constants/api_constants.js
@@ -5,10 +5,13 @@ const constants = {
   emailAlreadyExistsError: 'Email already exists',
   emailFormatInvalidError: 'Email format invalid',
   weakPasswordError: 'Password is not strong enough',
+  resourceNotFoundError: 'Resource not found',
+  forbiddenResourceError: 'Resource access forbidden',
   accountNotFoundError: 'Account not found',
   accountRegistrationSuccess: 'Account registered successfully',
   deviceNotFoundError: 'Device not found',
   clientNotFoundError: 'Client not found',
+  userNotFoundError: 'User not found',
   zoneNotFoundError: 'Zone not found',
   partitionNotFoundError: 'Partition not found',
   duplicateFoundError: 'Duplicate entry found',
@@ -16,10 +19,12 @@ const constants = {
   zoneDeletionSuccess: 'Zone deleted successfully',
   clientDeletionSuccess: 'Client deleted successfully',
   partitionDeletionSuccess: 'Partition deleted successfully',
+  userDeletionSuccess: 'User deleted successfully',
   noZoneFound: 'No zone found',
   noClientFound: 'No client found',
   noDeviceFound: 'No device found',
   noPartitionFound: 'No partition found',
+  noUserFound: 'No user found',
 };
 
 export default constants;
